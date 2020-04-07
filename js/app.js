84a68b6112b297f2f73260634e363ce7b1f06edc
sandracrz1 <48675455+sandracrz1@users.noreply.github.com>
2020-04-07 14:03:11 +0200

    change css js

--- js/app.js
+++ js/app.js
@@ -2,52 +2,57 @@
 
 var menuInstance;
 var collapsibleInstance;
+var collapsibleElems;
 var menuElems;
 var openMenuItem;
 var navItem;
+var navItemArticleHtml;
 
 
+navItemArticleHtml = document.querySelectorAll(".nav_item_article_click");
 openMenuItem = document.querySelectorAll(".collapsible-header");
 navItem = document.querySelectorAll(".nav_item_click");
-console.log(navItem);
+
 
 //Initialization materialize js sidenav
-document.addEventListener('DOMContentLoaded', function() {
-   menuElems = document.querySelectorAll('.sidenav');
+document.addEventListener('DOMContentLoaded', function () {
+  menuElems = document.querySelectorAll('.sidenav');
   M.Sidenav.init(menuElems);
   menuInstance = M.Sidenav.getInstance(menuElems[0]);
-  var collapsibleElems = document.querySelectorAll('.collapsible');
+  collapsibleElems = document.querySelectorAll('.collapsible');
   M.Collapsible.init(collapsibleElems);
-  collapsibleInstance = M.Collapsible.getInstance(collapsibleElems[0]);
+  collapsibleInstance = M.Collapsible.getInstance(collapsibleElems[1]);
 });
 
 // open-close sidenav
-document.getElementById("menu").addEventListener("click", function() {
+document.getElementById("menu").addEventListener("click", function () {
   menuInstance.open();
 })
-document.querySelector(".close").addEventListener("click", function(e){
+document.querySelector(".close").addEventListener("click", function (e) {
   e.stopPropagation();
   menuInstance.close();
 })
 //close item sidenav
-navItem.forEach(function(item) {
-  item.addEventListener("click", function(e) {
+navItem.forEach(function (item) {
+  item.addEventListener("click", function (e) {
     e.stopPropagation();
     menuInstance.close();
-  }) 
+  })
 })
-openMenuItem.forEach(function(e){
-  e.addEventListener("click", function(event){
- var a = event.target.children[0];
-  if (a.innerHTML === "arrow_downward") {
+//close sous nav article
+navItemArticleHtml.forEach(function (e) {
+  e.addEventListener("click", function (event) {
+    collapsibleInstance.close();
+  })
+})
+//change arrow direction
+openMenuItem.forEach(function (e) {
+  e.addEventListener("click", function (event) {
+    var a = event.target.children[0];
+    if (a.innerHTML === "arrow_downward") {
       a.innerHTML = "arrow_upward";
-  } else {
+    } else {
       a.innerHTML = "arrow_downward";
-  }})
- })
- 
-
-  
- 
-  
-
+    }
+  })
+})
